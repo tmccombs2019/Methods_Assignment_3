44f17470c27a99fdcca269938fd285a8983d5585
tmccombs2019 <tracey.mccombs@verizon.net>
2019-01-31 22:54:31 -0500

    v1

--- Methods_Assignment_3/Program.cs
+++ Methods_Assignment_3/Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/*
+ * Author: Tracey McCombs
+ * Date: 1/31/2019
+ * Comments: This C# console application code will show the use of methods after getting input from users. There are two methods: one value return method and one void method. 
+ */
+
+using System;
 
 namespace Methods_Assignment_3
 {
@@ -6,7 +12,40 @@ namespace Methods_Assignment_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+            Console.WriteLine("Please enter your full name.");
+
+            /*Use try catch to validate users input.
+             * If bad input in entered the catch block will handle the error and a message will display.
+             */
+          
+            try
+            {
+                // this variable will gather data from user
+                string input = Console.ReadLine();
+
+                Console.WriteLine("Hello!" + input);
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey(true);
+
+            }// end of try
+
+            catch
+            {
+                Console.WriteLine("Please enter your name next time...");
+                Console.WriteLine("Press any key to exit the program...");
+                Console.ReadKey(true);
+            }// end of catch
+        }//end of static void main
+
+        /* this is the static value return method -  it cannot be instantiated with the key word "new".
+         * The accessor is private which means the method is limited to the containing class
+        */
+        //private static void Hello_name(string value_of_input);
+       // {
+
+      //  }
+
+        
+
+    }//end of class
+}//end of namespace
